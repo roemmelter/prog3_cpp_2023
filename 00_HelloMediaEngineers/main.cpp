ed1a751f58f492d3b866d2d0356afc6d3ae6e9a9
Erik Roemmelt <28647227+roemmelter@users.noreply.github.com>
2023-10-30 06:04:09 +0100

    main.cpp ~ Lab-00 done

---
+++ 00_HelloMediaEngineers/main.cpp
@@ -0,0 +1,12 @@
+/*
+    Schreibe ein Programm, welches "Hello Media Engineers" in der Konsole ausgibt.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Media Engineers" << endl;
+
+    return 0;    
+}
